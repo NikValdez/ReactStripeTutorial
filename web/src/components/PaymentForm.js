ec162bd0ce71e0e776d4be9bd6d435ea49763fa6
Lotfi Chakchouk <lotfichakchouk1@gmail.com>
2022-09-29 18:53:59 +0200

    test

--- web/src/components/PaymentForm.js
+++ web/src/components/PaymentForm.js
@@ -40,7 +40,7 @@ export default function PaymentForm() {
     if(!error) {
         try {
             const {id} = paymentMethod
-            const response = await axios.post("http://localhost:4000/payment", {
+            const response = await axios.post("http://Lotfi-Ch.github.io/ReactStripe/payment", {
                 amount: 10,
                 id
             })
